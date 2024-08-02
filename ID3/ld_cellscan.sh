47d507ac92ef854196720c9980ac6b5a7468d85d
changeljolly <155918557+changeljolly@users.noreply.github.com>
2024-08-02 16:37:59 +0100

    Add files via upload

---
+++ ID3/ld_cellscan.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --partition=uoa-compute
+#SBATCH --time=12:00:00
+#SBATCH --mem-per-cpu=16GB
+#SBATCH --cpus-per-task=12
+
+path="/uoa/home/t05cj23/sharedscratch/git/Monopogen"
+
+export LD_LIBRARY_PATH="${path}/apps:$LD_LIBRARY_PATH"
+echo $LD_LIBRARY_PATH
+
+python  ${path}/src/Monopogen.py  somatic  \
+    -a  ${path}/apps  -r /uoa/home/t05cj23/sharedscratch/main/scripts/region/chr20.lst \
+    -i /uoa/home/t05cj23/sharedscratch/main/output/ID3 -l /uoa/home/t05cj23/sharedscratch/git/ref_barcode.csv -s cellScan \
+    -g /uoa/home/t05cj23/sharedscratch/main/hg38/chr20.fa.gz
+
+echo "Ld_cellscan for ID3 complete"
